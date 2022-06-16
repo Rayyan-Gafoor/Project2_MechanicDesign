07e4801a72beb22b2a95adcba6af7f8663f5c570
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2022-06-16 20:40:12 +0200

    title Menu

--- Mechanical Design/Assets/LoadLevelScript.cs
+++ Mechanical Design/Assets/LoadLevelScript.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadLevelScript : MonoBehaviour
 {
+  
+    
     private void OnTriggerEnter(Collider other)
     {
         if(other.name== "ValveMC")
@@ -12,4 +15,9 @@ public class LoadLevelScript : MonoBehaviour
             SceneManager.LoadScene(sceneName: "Level 2");
         }
     }
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene(sceneName: "");
+    }
+    
 }
