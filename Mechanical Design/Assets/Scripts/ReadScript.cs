e8d0bb4337adae77ba5ae0ed51ff923d983a50d9
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2022-06-16 18:17:45 +0200

    tutorial implemented

--- Mechanical Design/Assets/Scripts/ReadScript.cs
+++ Mechanical Design/Assets/Scripts/ReadScript.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class ReadScript : MonoBehaviour
 {
     public int flag = 0;
+    public int flag__tut=0;
     public GameObject temp__letter;
     public GameObject pickup__ui;
+    public GameObject tutorial;
     public bool can__read = false;
     private void Start()
     {
         
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(close__tutorial());
+        }
+        if (Input.GetKeyDown(KeyCode.E) && flag == 1)
+        {
+            StartCoroutine(close());
+        }
     }
     private void OnTriggerStay(Collider other)
     {
@@ -33,13 +46,14 @@ public class ReadScript : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        can__read = true;
+       //can__read = true;
         pickup__ui.SetActive(false);
-        if (other.tag == "Player")
+        /*if (other.tag == "Player")
         {
             temp__letter.SetActive(false);
             flag = 0;
-        }
+            StartCoroutine(display__tutorial());
+        }*/
     }
     IEnumerator read()
     {
@@ -54,7 +68,23 @@ public class ReadScript : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         Debug.Log("close letter");
         temp__letter.SetActive(false);
+        StartCoroutine(display__tutorial());
         flag = 0;
        
+    }
+    IEnumerator display__tutorial()
+    {
+        yield return new WaitForSeconds(0.5f);
+        tutorial.SetActive(true);
+        flag__tut =1;
+        yield return new WaitForSeconds(5);
+        StartCoroutine(close__tutorial());
+    }
+    IEnumerator close__tutorial()
+    {
+        yield return new WaitForSeconds(0.5f);
+        tutorial.SetActive(false);
+        flag__tut = 0;
+        yield break;
     }
 }
